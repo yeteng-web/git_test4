6d57110972728971bc94e828bbe5b2b12ec3811b
yeteng <850341462@qq.com>
2025-06-22 19:10:17 +0800

    第一次

---
+++ a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // [1]()[2]() 引入标准输入输出库
+ 
+int main() {        // [3]() 程序入口函数 
+    printf("Hello World");  // [1]()[3]() 输出文本到控制台
+    // joke
+    return 0;       // [2]() 表示程序正常结束 
+}
